972921316d832316f1e20272f70aabc5eea5d5ca
Benjamin Manford <benjaminmanford@gmail.com>
2019-12-20 15:15:52 +0800

    Delete unused facade, classes and rename LaravelUssdServiceProvider.php to UssdServiceProvider.php

---
+++ src/UssdServiceProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace CyberSai\LaravelUssd;
+
+use CyberSai\LaravelUssd\Console\Commands\CreateController;
+use CyberSai\LaravelUssd\Console\Commands\CreateListView;
+use CyberSai\LaravelUssd\Console\Commands\CreateMigration;
+use CyberSai\LaravelUssd\Console\Commands\CreateModel;
+use CyberSai\LaravelUssd\Console\Commands\CreateSimpleView;
+use CyberSai\LaravelUssd\Console\Commands\CreateTitledView;
+use Illuminate\Support\ServiceProvider;
+
+class UssdServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole())
+        {
+            $this->publishes([
+                __DIR__ . '/../config/ussd.php' => config_path('ussd.php'),
+            ]);
+
+            $this->commands([
+                CreateMigration::class,
+                CreateModel::class,
+                CreateSimpleView::class,
+                CreateTitledView::class,
+                CreateListView::class,
+                CreateController::class
+            ]);
+        }
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/ussd.php', 'config');
+    }
+}
